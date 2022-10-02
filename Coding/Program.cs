fd428f354e93083f5a9ffb16987a277e75b67242
Murad Mammadzade <mammadzade03@gmail.com>
2022-10-02 12:22:34 +0400

    Modified Program.cs file

--- Coding/Program.cs
+++ Coding/Program.cs
@@ -7,20 +7,10 @@ using System.Reflection;
 
 namespace Coding
 {
-    internal class Program
+    public class Program
     {
-        static int Result(int[] arr,int k)
-        {
-            Array.Sort(arr);
-            return arr[arr.Length-k];
-        }
         static void Main(string[] args)
         {
-            int[] arr = { 4, 2, 9, 7, 5, 6, 7, 1, 3 };
-            Console.WriteLine("Please enter k:");
-            int k=int.Parse(Console.ReadLine());
-            Console.WriteLine(Result(arr,k));
-            Console.ReadLine();
         }
     }
 }
