21f1b3852483fb47883c3db3d4e21413af49b91e
Murad Mammadzade <mammadzade03@gmail.com>
2022-10-02 11:40:02 +0400

    Add some functionality to my codes in Anagrams class

--- Coding/Problem1.Anagrams/Anagrams.cs
+++ Coding/Problem1.Anagrams/Anagrams.cs
@@ -8,15 +8,17 @@ namespace Coding.Problem1.Anagrams
 {
     public class Anagrams
     {
-        //Main Program Code
-        //Console.WriteLine("Please enter first string:");
-        //string first=Console.ReadLine();
-        //Console.WriteLine("Please enter second string:");
-        //string second = Console.ReadLine();
-        //var solution=ResultChecker(first, second) ==
-        //    true ? "The strings is anagram":
-        //    "The strings is not anagram";
-        //Console.WriteLine(solution);
+        /*Main Program Code
+        Console.WriteLine("Please enter first string:");
+        string first = Console.ReadLine();
+        Console.WriteLine("Please enter second string:");
+        string second = Console.ReadLine();
+        var solution = ResultChecker(CheckLengthForAnagram,
+            SortStrings,first, second) ==
+            true ? "The strings is anagram" :
+            "The strings is not anagram";
+        Console.WriteLine(solution);
+        */
         static bool CheckLengthForAnagram(string first, string second)
         {
             var result = first.Length != second.Length ? false : true;
@@ -29,10 +31,12 @@ namespace Coding.Problem1.Anagrams
 
             return new string(characters);
         }
-        static bool ResultChecker(string first, string second)
+        static bool ResultChecker(Func<string,string,bool> firstMethod,
+            Func<string,string> secondMethod, string first, string second)
         {
-            var result = (CheckLengthForAnagram(first, second) &&
-                SortStrings(first).Equals(SortStrings(second))) ? true : false;
+            var result = (firstMethod.Invoke(first, second) &&
+                secondMethod.Invoke(first).Equals
+                (secondMethod.Invoke(second))) ? true : false;
             return result;
         }
     }
