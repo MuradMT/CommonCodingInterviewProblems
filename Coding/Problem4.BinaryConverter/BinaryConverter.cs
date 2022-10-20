9ce332141a81e48cb11e0e22b6fac607e96fc009
Murad Mammadzade <mammadzade03@gmail.com>
2022-10-20 21:36:08 +0400

    Binary converter

---
+++ Coding/Problem4.BinaryConverter/BinaryConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace Coding.Problem4.BinaryConverter
+{
+    public class BinaryConverter
+    {
+        //        Console.Write("Please enter number:");
+        //            int orgnumber = int.Parse(Console.ReadLine());
+        //        int number = orgnumber;
+        //        int n = Convert.ToInt32(Math.Log(number, 2));
+        //        string binary = string.Empty;
+        //            for (int i =n; i >=0; i--)
+        //            {
+        //                if (number >= Convert.ToInt32(Math.Pow(2, i)))
+        //                {
+        //                    number-=Convert.ToInt32(Math.Pow(2, i));
+        //                    binary += "1";
+        //                }
+        //                else
+        //                {
+        //                    binary += "0";
+        //                }
+        //            }
+
+        //            Console.WriteLine($"the binary form of {orgnumber} is {binary}");
+        //Console.ReadLine();
+    }
+}
--- Coding/Program.cs
+++ Coding/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Xml.Linq;
 
 namespace Coding
 {
+    
+   
     public class Program
     {
+       
         static void Main(string[] args)
         {
+            
+            Console.ReadLine();
         }
     }
 }
