ebecc9dc06ed3d79091a67ab8ba7b56d27d03fc7
Murad Mammadzade <mammadzade03@gmail.com>
2022-11-10 12:04:11 +0400

    Solve the problem which you get string from console and do mathematical operations on this and you get the result of this operation

---
+++ Coding/Problem5.ConvertStringGivenConsoleToMath/Solution.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding.Problem5.ConvertStringGivenConsoleToMath
+{
+    public class Solution
+    {
+        /*Main Program Code
+         * In this program we do mathematical operations on console,
+         * after we can get the value of this operations.
+         * There are 2 solution for this problem,
+         * first i use ready way:
+         * 1)var text=GetText();
+         * DataTable dt = new DataTable();
+         * int answer = (int)dt.Compute(text,"");
+         * Console.WriteLine(answer);
+         * Console.ReadLine();
+         * Second i write solution from scratch,
+         * i use different logic in every method,
+         * this helps me to solve problem
+         * only using 3 line code on Main Program
+         * 2)var text=GetText();
+         * Result(text);
+         * Console.ReadLine();
+         */
+        static bool LengthChecker(string text)
+        {
+            if (text.Length % 2 == 0) return false;
+            return true;
+        }
+        static bool IsNumber(char symbol)
+        {
+            if (symbol >= '0' && symbol <= '9') return true;
+
+            return false;
+        }
+        static bool IsSymbol(char symbol)
+        {
+            var chars = new char[] { '+', '-', '*', '/' };
+            if (chars.Contains(symbol)) return true;
+            return false;
+        }
+        static bool TextChecker(string text)
+        {
+            if (!LengthChecker(text)) return false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (!IsNumber(text[i])) return false;
+                }
+                else
+                {
+                    if (!IsSymbol(text[i])) return false;
+                }
+            }
+            return true;
+        }
+        static List<string> StringSplitter(string text)
+        {
+
+            List<string> arr = text.Split(new char[] { '+' }).ToList();
+            List<string> arr2 = new List<string>(arr);
+            arr.Sort();
+            arr2.Sort();
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i].Contains('-'))
+                {
+                    var helper = arr2[i];
+                    arr2.Remove(arr2[i]);
+                    var operation = helper.Split('-');
+                    for (int j = 0; j < operation.Length; j++)
+                    {
+                        if (j != 0) operation[j] = "-" + operation[j];
+                        arr2.Add(operation[j]);
+                    }
+                }
+            }
+
+
+            return arr2;
+        }
+        static int ResultGenerator(string text)
+        {
+            int result = 0;
+            bool val = false;
+            if (text[0] == '-')
+            {
+                val = true;
+                text = text.Substring(1, text.Length - 1);
+            }
+            for (int i = 1; i < text.Length; i += 2)
+            {
+                switch (text[i])
+                {
+                    case '+':
+                        if (i == 1)
+                        {
+                            result = int.Parse(text[i - 1].ToString()) + int.Parse(text[i + 1].ToString());
+                        }
+                        else
+                        {
+                            result = result + int.Parse(text[i + 1].ToString());
+                        }
+                        break;
+                    case '-':
+                        if (i == 1)
+                        {
+                            result = int.Parse(text[i - 1].ToString()) - int.Parse(text[i + 1].ToString());
+                        }
+                        else
+                        {
+                            result = result - int.Parse(text[i + 1].ToString());
+                        }
+                        break;
+                    case '*':
+                        if (i == 1)
+                        {
+                            result = int.Parse(text[i - 1].ToString()) * int.Parse(text[i + 1].ToString());
+                        }
+                        else
+                        {
+                            result = result * int.Parse(text[i + 1].ToString());
+                        }
+                        break;
+                    case '/':
+                        if (i == 1)
+                        {
+                            result = int.Parse(text[i - 1].ToString()) / int.Parse(text[i + 1].ToString());
+                        }
+                        else
+                        {
+                            result = result / int.Parse(text[i + 1].ToString());
+                        }
+                        break;
+                }
+            }
+            return val == true ? result * -1 : result;
+        }
+        static int CountGenerator(string text)
+        {
+            int count = 0;
+            int res;
+            var gen = int.TryParse(text, out res);
+            if (gen)
+            {
+                count += res;
+            }
+            else
+            {
+                count += ResultGenerator(text);
+            }
+            return count;
+
+        }
+        static void Result(string text)
+        {
+            if (TextChecker(text))
+            {
+                int count = 0;
+                var res = StringSplitter(text);
+                foreach (var item in res)
+                {
+                    count += CountGenerator(item);
+                }
+                Console.WriteLine(count);
+
+            }
+            else
+            {
+                Console.WriteLine("Please enter right format");
+            }
+        }
+        static string GetText()
+        {
+            var tex = Console.ReadLine().Trim().Split();
+            string text = string.Empty;
+            foreach (var item in tex)
+            {
+                text += item;
+            }
+            return text;
+        }
+
+
+    }
+}
--- Coding/Program.cs
+++ Coding/Program.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO.MemoryMappedFiles;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Numerics;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Coding
 {
@@ -16,11 +19,68 @@ namespace Coding
    
     public class Program
     {
-       
+        #region Mesele10
+        //static string Result(string x, char[]arr)
+        // {
+        //     string a = default;
+        //     string b = default;
+        //     string def=default;
+        //     if (x.Length != 3)
+        //     {
+        //         return "Emeliyyat dogru olmalidir";
+        //     }
+        //     else
+        //     {
+        //         for (int i = 0; i < 3; i++)
+        //         {
+
+        //             if (!arr.Contains(x[i]) && x[i] >= '0' && x[i] <= '9')
+        //             {
+        //                 if (i == 0)
+        //                 {
+        //                     a = x[i].ToString();
+        //                 }
+        //                 else if (i == 2)
+        //                 {
+        //                     b = x[i].ToString();
+        //                 }
+
+        //             }
+        //             else if (arr.Contains(x[i]))
+        //             {
+        //                 def = x[i].ToString();
+        //             }
+        //             else
+        //             {
+        //                 return "Emeliyyatda ancaq reqemler islenmelidir";
+        //             }
+        //         }
+        //         switch (def)
+        //         {
+        //             case "+":
+        //                 return $"{Convert.ToInt32(a) + Convert.ToInt32(b)}";
+        //             case "-":
+        //                 return $"{Convert.ToInt32(a) - Convert.ToInt32(b)}";
+        //             case "*":
+        //                 return $"{Convert.ToInt32(a) * Convert.ToInt32(b)}";
+        //             case "/":
+        //                 return $"{Convert.ToInt32(a) / Convert.ToInt32(b)}";
+        //         }
+        //     }
+        //     return "This is default";
+
+        //}
+        #endregion
+        
         static void Main(string[] args)
         {
-            
-            Console.ReadLine();
+            #region Mesele10Main
+            //char[] chars = {'+','-','*','/' };
+            //string x = Console.ReadLine();
+            //var res = Result(x, chars);
+            //Console.WriteLine(res);
+            #endregion
+            //Console.ReadLine();
         }
     }
 }
