7aba32e69e56fea1383618efbf15eeaadb55e6e3
Murad Mammadzade <mammadzade03@gmail.com>
2023-01-10 21:26:05 +0400

    few modification

--- Coding/Program.cs
+++ Coding/Program.cs
@@ -1,22 +1,13 @@
-﻿using System;
+﻿global using Coding.Problem1;
+using Coding.Problem1.Anagrams;
+using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.IO.MemoryMappedFiles;
 using System.Linq;
-using System.Net.Http.Headers;
-using System.Numerics;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Xml.Linq;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Coding
 {
-    
-   
+
+
     public class Program
     {
         #region Mesele10
@@ -70,8 +61,75 @@ namespace Coding
         //     return "This is default";
 
         //}
+        //static double Checker(string[] x, string[] y)
+        //{
+        //    if (x.Contains("AD") && y.Contains("AD"))
+        //    {
+        //        return Math.Abs(double.Parse(y[1]) - double.Parse(x[1]));
+        //    }
+        //    else if (x.Contains("BC") && y.Contains("AD"))
+        //    {
+        //        return Math.Abs(double.Parse(y[1]) - (double.Parse(x[0]) - 1) * -(1));
+        //    }
+        //    else if (x.Contains("AD") && y.Contains("BC"))
+        //    {
+        //        return Math.Abs((double.Parse(y[0]) - 1) * (-1) - (double.Parse(x[1])));
+        //    }
+        //    else if (x.Contains("BC") && y.Contains("BC"))
+        //    {
+        //        return Math.Abs((double.Parse(y[0]) - 1) * (-1) - (int.Parse(x[0]) - 1) * (-1));
+        //    }
+        //    return 0;
+        //}
+
+        //static (int,int) Combination(int a,int b,int c)
+        //{
+        //    //1700 500 200
+        //    if (a % b == 0)
+        //    {
+        //        return (a / b, 0);
+        //    }
+        //    else if(a % c == 0)
+        //    {
+        //        return (a/c, 0);
+        //    }
+        //    else
+        //    {
+        //        var max = b > c ? b : c;
+        //        var min = b < c ? b: c;
+        //        var temp = a;
+        //        var res=Math.Round((double)a / max);
+        //        temp -= (int)res*max;
+        //        var rs=Math.Round((double)a / min);
+        //        temp -= (int)rs*min;
+        //        if(temp == 0)
+        //        {
+        //            return ((int)res, (int)rs);
+        //        }
+        //        else
+        //        {
+        //            var res1= Math.Round((double)a / min);
+        //            var res2= Math.Round((double)a / max);
+        //            return ((int)res1, (int)res2);
+        //        }
+
+        //    }
+        //    return (0, 0);
+        //}
+        //static string StringGetter(string emp)
+        //{
+        //    string res = string.Empty;
+        //    for (int i = emp.Length - 1; i >= 0; i--)
+        //    {
+        //        res += emp[i];
+        //    }
+
+        //    return res;
+        //}
         #endregion
-        
+
+
+
         static void Main(string[] args)
         {
             #region Mesele10Main
@@ -79,8 +137,62 @@ namespace Coding
             //string x = Console.ReadLine();
             //var res = Result(x, chars);
             //Console.WriteLine(res);
+            //int n=int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        if (i == 0 || i ==2)
+            //        {
+            //            Console.Write("*");
+            //        }
+            //        else
+            //        {
+            //            if(j== 0 || j == n - 1)
+            //            {
+            //                Console.Write("*");
+            //            }
+            //            else
+            //            {
+            //                Console.Write(" ");
+            //            }
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+            //while (true)
+            //{
+            //    var x = Console.ReadLine().Split();
+            //    var y = Console.ReadLine().Split();
+            //    Console.WriteLine(Checker(x, y));
+            //    var res=Console.ReadLine();
+            //    if (res == "y")
+            //    {
+            //        continue;
+            //    }
+            //    else if (res == "n")
+            //    {
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine("Please enter numbers:");
+            //var n=int.Parse(Console.ReadLine());
+            //var m = int.Parse(Console.ReadLine());
+            //var c = int.Parse(Console.ReadLine());
+            //var res=Combination(n, m, c);
+            //Console.WriteLine("The result is:");
+            //Console.WriteLine(res.Item1+" "+res.Item2);
+
             #endregion
-            //Console.ReadLine();
+            #region Elave
+            //int num = int.Parse(Console.ReadLine());
+            //var res = num.ToString().ToCharArray().OrderBy(x => x);
+            //Console.WriteLine(int.Parse(new string(res)));
+            #endregion
+            int result = default(int);
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
